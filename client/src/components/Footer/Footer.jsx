5cef9878fb63be9a87f9aa694385ebf280414f3e
gechjs <gizachew980@gmail.com>
2024-10-08 22:15:39 -0700

    make the footer and header page responsive

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,24 +1,20 @@
 import React from "react";
 import classes from "./Footer.module.css";
 import footerLogo from "../../assets/logo.png";
-
+import { AiFillInstagram } from "react-icons/ai";
+import { FaYoutube } from "react-icons/fa";
+import { FaFacebookF } from "react-icons/fa";
 function Footer() {
   return (
     <footer>
       <div className={classes.footer__container}>
         <div className={classes.footer__logo}>
           <img src={footerLogo} alt="Evangadi Logo hgchjhgvhjgcghc" />
-        </div>
-        <div className={classes.footer__social}>
-          <a href="#">
-            <i className="fab fa-facebook-f"></i>
-          </a>
-          <a href="#">
-            <i className="fab fa-instagram"></i>
-          </a>
-          <a href="#">
-            <i className="fab fa-youtube"></i>
-          </a>
+        <div className={classes.social_icons}>
+            <div><FaFacebookF size={25}/></div>
+            <div><AiFillInstagram size={30}/></div>
+            <div><FaYoutube size={30}/></div>
+          </div>
         </div>
         <div className={classes.footer__links}>
           <h3>Useful Link</h3>
