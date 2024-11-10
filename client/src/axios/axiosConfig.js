79bbe74f93b0efb6ec6a3197db0b1130cb8d6d3e
ibsa-eng <104551852+ibsa-eng@users.noreply.github.com>
2024-11-09 18:23:04 -0600

    initial commit

--- client/src/axios/axiosConfig.js
+++ client/src/axios/axiosConfig.js
@@ -1,9 +1,9 @@
 import axios from "axios";
-const port = import.meta.env.VITE_PORT
-
+const port = import.meta.env.VITE_PORT;
 
 const axiosInstance = axios.create({
-  baseURL: `http://localhost:${port}/api`,
+  // baseURL: `http://localhost:${port}/api`,
+  baseURL: `https://evangadi-forum-backend-20-qqbn.onrender.com/api`,
 });
 
 export default axiosInstance;
